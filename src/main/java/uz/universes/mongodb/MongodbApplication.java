c7e29cf504df85a6dc7e1701a4f767bd863930cb
Rasulmatov <rasulmatovmuhammad@gmail.com>
2024-09-10 21:11:23 +0500

    WebClient praktika

--- src/main/java/uz/universes/mongodb/MongodbApplication.java
+++ src/main/java/uz/universes/mongodb/MongodbApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
@@ -14,12 +15,16 @@ public class MongodbApplication {
         SpringApplication.run(MongodbApplication.class,args);
     }
 
-    @Bean
+    /*@Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
         return restTemplateBuilder
                 .setConnectTimeout(Duration.ofMillis(2000))
                 .setReadTimeout(Duration.ofMillis(2000))
                 .build();
+    }*/
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
     }
 
 }
--- src/main/java/uz/universes/mongodb/comment/CommentResource.java
+++ src/main/java/uz/universes/mongodb/comment/CommentResource.java
@@ -3,9 +3,13 @@ package uz.universes.mongodb.comment;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import uz.universes.mongodb.post.PostDto;
 
 import java.util.Collections;
@@ -14,23 +18,26 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CommentResource {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
     @Value("${comments.url.postComments}")
     private String postCommentsURL;
     @Value("${comments.url.saveComments}")
     private String saveCommentsURL;
 
     public List<CommentDTO> getAllComments(@NonNull Integer id){
-        try {
-            List comments= restTemplate.getForObject(postCommentsURL, List.class, id);
-            return comments;
-        }catch (ResourceAccessException e){
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
+        return webClient.get()
+                .uri(postCommentsURL,id)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<CommentDTO>>() {})
+                .block();
     }
     public void saveAll(List<CommentCreateDTO> commentCreateDTOS){
-        restTemplate.postForObject(saveCommentsURL,commentCreateDTOS,Void.class);
+        webClient.post()
+                .uri(saveCommentsURL)
+                .body(BodyInserters.fromValue(commentCreateDTOS))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
     }
 
 }
