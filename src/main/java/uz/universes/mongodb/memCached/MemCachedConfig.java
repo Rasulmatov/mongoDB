ecfc96965ffa9bdf06a836a49cce0c89945cb714
Rasulmatov <rasulmatovmuhammad@gmail.com>
2024-09-18 17:41:49 +0500

    zor dars

--- src/main/java/uz/universes/mongodb/memCached/MemCachedConfig.java
+++ src/main/java/uz/universes/mongodb/memCached/MemCachedConfig.java
@@ -2,17 +2,9 @@ package uz.universes.mongodb.memCached;
 
 import net.rubyeye.xmemcached.MemcachedClient;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uz.universes.mongodb.entity.Student;
-
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableCaching
